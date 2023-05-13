5c5c6005437e5b246d1fc4f823c911317f543a04
Uchenna Obicheta <uobicheta@hotmail.com>
2023-05-13 14:35:42 -0400

    Updated list of countries

--- README.md
+++ README.md
@@ -15,13 +15,13 @@ Word Jumble is a game with a set of words where each word is scrambled.  The pla
 - Tkinter (and, since Python 3.1, ttk, the interface to the newer themed widgets) is included in the Python standard library.  It 
   is highly recommend installing Python using the standard binary distributions from python.org. These will automatically install Tcl/Tk, which is needed by Tkinter.  
 - Set environment PATH variable to point to location of python.exe. 
-- Open a terminal and issue the following command to run the Word Jumble game:
+- Open a terminal and issue the following command to run the Word Jumble game:  
 
 python main.py 
 
-Or
+Or  
 
-<Full path to python executable> main.py (i.e. C:\python39\python.exe main.py)
+<Full path to python executable> main.py (i.e. C:\python39\python.exe main.py)  
 
 ```
 
--- main.py
+++ main.py
@@ -3,10 +3,115 @@ from random import choice
 from random import shuffle
 from tkinter import messagebox
 
+
 root = Tk()
 root.title('Word Jumble')
 root.geometry("600x400")
 root.iconbitmap("./icon.ico")
+
+# Create label to display scrambled word
+myLabel = Label(root, text="", font=("Helvetica", 50))
+myLabel.pack(pady=20)
+
+# Initialize game score and time left
+score = 0
+timeleft = 120
+
+
+def startGame(event):
+    """
+     This function starts the timer countdown, and display the scrambled word
+    """
+    if timeleft == 60:
+        countdown()
+        shuffler()
+        scorelabel.config(text="Score: 0")
+    else:
+        answer()
+
+
+def countdown():
+    global timeleft
+    if timeleft == 0:
+        messagebox.showinfo(
+            "Time Over", "Time is over and your score is " + str(score))
+    if timeleft > 0:
+        timeleft -= 1
+        timelabel.config(text="Time Left: " + str(timeleft))
+        timelabel.after(1000, countdown)
+
+
+def shuffler():
+    eAnswer.delete(0, END)
+    # ansLabel.config(text="")
+    ansLabel.after(400, lambda: ansLabel.config(text=''))
+
+    global word
+    Countries = ['Egypt', 'Sudan', 'Oman', 'Yemen', 'Syria', 'Algeria', 'Qatar', 'Lebanon', 'Tunisia', 'Libya',
+                 'Kuwait', 'Jordan', 'Iraq', 'France', 'Germany', 'Spain', 'China', 'India', 'Canada', 'Brazil',
+                 'Italy', 'Poland', 'Sweden', 'Norway', 'Greece', 'Netherlands', 'Japan', 'Iran', 'Portugal', 'Mexico',
+                 'Cuba', 'Argentina', 'Colombia', 'Nigeria', 'South Africa', 'Ghana', 'United States of America', 'Liberia',
+                 'Uganda', 'Cameroon', 'Kenya', 'Algeria', 'Mali', 'Somalia', 'Zimbabwe', 'Sierra Leone', 'Congo',
+                 'Central African Republic', 'Mauritania', 'Libya', 'Togo', 'Tunisia', 'Gabon', 'Guinea-Bissau', 'Equatorial Guinea',
+                 'Comoros', 'Seychelles', 'Djibouti', 'Sao Tome & Principe', 'Cabo Verde', 'Botswana']
+
+    word = choice(Countries)
+
+    breakWord = list(word)
+    shuffle(breakWord)
+
+    global shuffled
+    shuffled = ''
+    for letter in breakWord:
+        shuffled += letter
+
+    myLabel.config(text=shuffled)
+
+
+def answer():
+    global score
+    global timeleft
+
+    if timeleft > 0:
+        eAnswer.focus_set()
+        if eAnswer.get().lower() == word.lower():
+            score += 1
+            ansLabel.config(text="Correct!")
+        else:
+            ansLabel.config(text="Incorrect")
+        scorelabel.config(text="Score: " + str(score))
+
+        shuffler()
+
+
+# Create label to display score
+scorelabel = Label(root, text="Enter to start", font=('Helvetica', 24))
+scorelabel.pack()
+
+# Create label to display remaining time left
+timelabel = Label(root, text="Time left: " +
+                  str(timeleft), font=('Helvetica', 12))
+timelabel.pack()
+
+# Create textbox to enter reconstructed word
+eAnswer = Entry(root, font=("Helvetica", 25))
+eAnswer.pack(pady=20)
+
+# Create frame to arrange other widgets
+myFrame = Frame(root)
+myFrame.pack(pady=20)
+
+# Create button to show answer when clicked
+ansButton = Button(myFrame, text="Answer!", command=answer)
+ansButton.grid(row=0, column=1, padx=10)
+
+# Create label to show whether the reconstructed word is correct or incorrect
+ansLabel = Label(root, text="", font=("Helvetica", 20))
+ansLabel.pack(pady=20)
+
+# Show new scrambled word when button is pressed
+root.bind('<Return>', startGame)
+eAnswer.focus_set()
 
 
 root.mainloop()
